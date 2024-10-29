ad1dfb731d0e76184c01ea9d5a4a4633d9f5850d
OjasJohar <ojasjohar@gmail.com>
2024-10-29 14:31:00 +0530

    Final commit!

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { Store } from './redux/Store';
 import {Provider} from "react-redux";
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,12 +12,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // Step:3 == Link redux with our react file.
 
 root.render(
-  <HashRouter basename='/ECOMSITE'>
-  
-  <Provider store={Store}>
+  <BrowserRouter>
+    <Provider store={Store}>
     <App />
     <Toaster/>
-  </Provider>
-  
-  </HashRouter>  
+    </Provider>
+  </BrowserRouter>  
 );
